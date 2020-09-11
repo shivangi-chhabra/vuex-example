5c294c08b83f0f81112fecd872856a93a0f443c0
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-09-11 23:19:06 +0530

    used store to export data

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,21 +2,20 @@
     <div id="product-list-one">
         <h2>Product List One</h2>
         <ul>
-             <li v-for="product in products"> 
-               <span class="name">{{ product.name }} </span>
-               <span class="price">{{ product.price }}</span>
-            </li>
+            <li v-for="product in products" :key = "product">
+                <span class="name">{{product.name}}</span>
+                <span class="price">${{product.price}}</span>
+            </li>    
         </ul>
     </div>
 </template>
 <script>
 export default {
-    props: ['products'],
-    data() {
-        return {
-
+    computed: {
+        products() {
+           return this.$store.state.products
         }
-    }
+    }   
 }
 </script>
 <style scoped>
--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -1,20 +1,19 @@
 <template>
     <div id = "product-list-two">
-        <h2>Product List One</h2>
+        <h2>Product List Two</h2>
         <ul>
-             <li v-for="product in products"> 
+             <li v-for="product in products" :key = "product"> 
                <span class="name">{{ product.name }} </span>
-               <span class="price">{{ product.price }}</span>
+               <span class="price">${{ product.price }}</span>
             </li>
         </ul>
     </div>
 </template>
 <script>
 export default {
-    props: ['products'],
-    data() {
-        return {
-
+    computed: {
+        products() {
+            return this.$store.state.products
         }
     }
 }
