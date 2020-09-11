19db337bc5f14d27bf6914db7e766ebb7af4d5ba
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-09-12 01:56:07 +0530

    use of getters

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
     <div id = "product-list-two">
         <h2>Product List Two</h2>
         <ul>
-             <li v-for="product in products" :key = "product"> 
+             <li v-for="product in saleProducts" :key = "product"> 
                <span class="name">{{ product.name }} </span>
                <span class="price">${{ product.price }}</span>
             </li>
@@ -14,7 +14,10 @@ export default {
     computed: {
         products() {
             return this.$store.state.products
-        }
+        },
+        saleProducts() {
+           return this.$store.getters.saleProducts
+        }       
     }
 }
 </script>
