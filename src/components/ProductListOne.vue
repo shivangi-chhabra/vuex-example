a5a30dca483d5275dcc8da33b48bedfb58f61aa3
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-09-14 01:53:25 +0530

    in action use of payload

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -7,7 +7,7 @@
                 <span class="price">${{product.price}}</span>
             </li>    
         </ul>
-        <button v-on:click="reducePrice">Reduce Price</button>
+        <button v-on:click="reducePrice(4)">Reduce Price</button>
     </div>
 </template>
 <script>
@@ -21,12 +21,12 @@ export default {
         }       
     },
     methods: {
-        reducePrice: function() {
+        reducePrice: function(amount) {
             /*this.$store.state.products.forEach(product => {
                 product.price -= 1       
             })*/
             // this.$store.commit('reducePrice')
-            this.$store.dispatch('reducePrice')
+            this.$store.dispatch('reducePrice',amount)
         }
     }   
 }
